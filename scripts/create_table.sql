8f6a4403181120a9656797f9ae229d1cf0be814d
Angad0691996 <angadbandal123@gmail.com>
2025-05-01 17:32:44 +0530

    added scripts like create MySQL table

---
+++ scripts/create_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS vehicle_status (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    vehicle_ID VARCHAR(50),
+    Speed INT,
+    timestamp VARCHAR(50),
+    location VARCHAR(100)
+);
